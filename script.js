7daf603dedd1b816aaae489b57887122c7d2418a
Narit Trikasemsak <ntrika25@colby.edu>
2023-02-25 16:13:04 -0500

    Website overhaul

--- script.js
+++ script.js
@@ -1,18 +1,52 @@
+let directory = ""; 
+let file = ""; 
+
 $('#terminal').terminal({
-    hello: function(what) {
-        this.echo('Hello, ' + what + '. Welcome to this terminal.');
-        // this string can be written with ES6 - uncomment to test
-        // this.echo(`Hello, ${what}. Wellcome to this terminal.`);
-    }, 
     pwd: function(){
-        this.echo('/home/ \n You are currently on the home page. To advance, find out where you can go. HINT: command to list all files')
+        this.echo('/home/ \n You are currently on the home page. Next step: where to? HINT: command to list all files')
     },
     ls: function(){
-        this.echo('home        page1 \npage2       page3 \n these are the files you can go to. \nHINT: go to page 1' )
+        this.echo('home        page1 \npage2       page3 \nThese are the files you can go to. \nHINT: go to page 1' )
     },
     cd: function(destination){
         if (destination == "page1") {
-            this.echo('nice!!')
+            this.exec(window.location.href="/page1.html")
+        }
+        else if (destination == "home") {
+            this.exec(window.location.href="/main.html")
+        }
+        else{
+            this.echo("Sorry - for the purposes of this demo please type cd page1")
+        }
+    },
+    mkdir: function(name){
+        directory = name
+        if(directory){
+            this.echo("New directory "  + file + " made!")
+        }
+
+    },
+    rmdir: function(remove){
+        if (remove == directory){
+            this.echo("You successfully removed the file!")
+        }
+        else{
+            this.echo("ERROR: No such file exists. Did you misspell the directory you wanted to delete?")
+        }
+    },
+    touch: function(name){
+        file = name
+        if(file){
+            this.echo("New file "  + file + " made!")
+        }
+
+    },
+    rm: function(remove){
+        if (remove == file){
+            this.echo("You successfully removed the file!")
+        }
+        else{
+            this.echo("ERROR: No such file exists. Did you misspell the file you wanted to delete?")
         }
     }
 }, {
